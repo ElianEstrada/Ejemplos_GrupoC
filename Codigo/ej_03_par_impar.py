fcca68718734af9792d46946a09a632844c7e9dc
Elian_Estrada <chictibiris@gmail.com>
2021-06-29 19:50:36 -0600

    primer commit

---
+++ Codigo/ej_03_par_impar.py
@@ -0,0 +1,9 @@
+
+numero = int(input("Ingrese un número: "))
+
+resultado = numero % 2
+
+if (resultado == 0):
+    print("El número es par")
+else: 
+    print("El número es impar")
---
+++ Codigo/ejemplo_sintaxis_01.py
@@ -0,0 +1,16 @@
+#esto es un cometario de una sola linea
+
+'''
+esto es un comentario multilinea
+es decir que puedo 
+escribir en mas de una liea
+'''
+
+"""
+esto es un comentario 
+multilinea nuevamentea
+"""
+
+# 'print' '(' expresión  ')'
+
+print("Hola Mundo")
