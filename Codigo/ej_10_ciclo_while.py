b886e6fe006eb4c0389d53e5cd6b25ecd16c2642
ElianEstrada <chictibiris@gmail.com>
2021-07-15 22:02:10 -0600

    Clase 10

---
+++ Codigo/ej_10_ciclo_while.py
@@ -0,0 +1,16 @@
+inicio = 1
+fin = 1000
+
+#sintaxys del while: 
+# 'while' '(' condicion ')' ':'
+
+while (inicio <= fin):
+    print(inicio)
+    #formas de incrementar
+    #1. suma directa
+    #inicio = inicio + 1
+
+    #2. operador sum_igual +=
+    inicio += 1
+
+print("Hasta luego")
---
+++ Codigo/ej_10_ciclo_while_input.py
@@ -0,0 +1,33 @@
+contador = 10
+
+final = int(input("Ingrese un número: "))
+
+while (contador >= final):
+    print(contador)
+    contador -= 1
+
+print("\nHasta luego")
+
+
+"""
+Realizar un programa que pida al usuario 
+el número inical y el final
+y mostrar todos los números 
+que estan entre el inicial y el final
+"""
+
+
+"""
+
+Mostrar la tabla de multiplicar 
+preguntandole al usuario el número de tabla 
+hasta que número quiere llegar
+
+Salida: 
+
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+
+"""
